46bf2f47d570c5ca7deffa64fbf467f66854a4d1
codersharon <124261315+codersharon@users.noreply.github.com>
2023-05-04 11:49:38 +0000

    tech-wave

--- pages/comps/navbar.tsx
+++ pages/comps/navbar.tsx
@@ -21,18 +21,18 @@ const Navbar = () => {
 			}}} className='bg-white rounded lg:hidden' src={'/menu.svg'} />
 			<Link href={'/'}><div className='mx-2 lg:rounded-none sm:rounded-xl bg-white'><div className='lg:rounded-none sm:rounded-xl cursor-default border-2 border-white text-2xl font-bold flex items-center justify-center bg-black'><h1 className='lg:rounded-none sm:rounded-xl p-2 px-5 border-2 border-black bg-white text-black'>Tech</h1><h1 className='lg:rounded-none sm:rounded-xl p-2 text-white'>Wave</h1></div></div></Link>
 			<ul className='sm:hidden lg:flex items-center justify-start p-5'>
-				<Link href={'/tech-news/'}><li className={router.pathname == '/tech-news'?`bg-white text-black p-2 rounded hover:text-black mx-2 cursor-pointer`: `p-2 rounded hover:bg-white hover:text-black mx-2 cursor-pointer`}>Tech News</li></Link>
-				<Link href={'/how-to/'}><li className={router.pathname == '/how-to'?`bg-white text-black p-2 rounded hover:text-black mx-2 cursor-pointer`: `p-2 rounded hover:bg-white hover:text-black mx-2 cursor-pointer`}>How to </li></Link>
-				<Link href={'/usefull-websites/'}><li className={router.pathname == '/usefull-websites'?`bg-white text-black p-2 rounded hover:text-black mx-2 cursor-pointer`: `p-2 rounded hover:bg-white hover:text-black mx-2 cursor-pointer`}>Usefull Websites</li></Link>
-				<Link href={'/usefull-apps/'}><li className={router.pathname == '/usefull-apps'?`bg-white text-black p-2 rounded hover:text-black mx-2 cursor-pointer`: `p-2 rounded hover:bg-white hover:text-black mx-2 cursor-pointer`}>Usefull Apps</li></Link>
+				<Link href={'/tech-news/'}><li className={router.pathname == '/tech-news'?`bg-white text-black p-2 rounded hover:text-black mx-2 cursor-pointer`: `p-2 rounded hover:bg-white hover:text-black mx-2 cursor-pointer`}>Tech-News</li></Link>
+				<Link href={'/how-to/'}><li className={router.pathname == '/how-to'?`bg-white text-black p-2 rounded hover:text-black mx-2 cursor-pointer`: `p-2 rounded hover:bg-white hover:text-black mx-2 cursor-pointer`}>How-to </li></Link>
+				<Link href={'/usefull-websites/'}><li className={router.pathname == '/usefull-websites'?`bg-white text-black p-2 rounded hover:text-black mx-2 cursor-pointer`: `p-2 rounded hover:bg-white hover:text-black mx-2 cursor-pointer`}>Usefull-Websites</li></Link>
+				<Link href={'/usefull-apps/'}><li className={router.pathname == '/usefull-apps'?`bg-white text-black p-2 rounded hover:text-black mx-2 cursor-pointer`: `p-2 rounded hover:bg-white hover:text-black mx-2 cursor-pointer`}>Usefull-Apps</li></Link>
 			</ul>
 			<ul className='flex items-center justify-center'><Link href={'/admin/admin'}><button className='cursor-pointer bg-red-600 hover:bg-red-700 text-white p-2 mx-5 rounded-xl'>Admin</button></Link></ul>
 		</nav>
 		<ul className={`bg-black text-white lg:hidden ${hamburger == false ? 'hidden' : 'flex flex-col'} items-center justify-start p-5`}>
-			<Link href={'/tech-news/'}><li className={router.pathname == '/tech-news'?`bg-white text-black p-2 rounded hover:text-black mx-2 cursor-pointer`: `p-2 rounded hover:bg-white hover:text-black mx-2 cursor-pointer`}>Tech News</li></Link>
-			<Link href={'/how-to/'}><li className={router.pathname == '/how-to'?`bg-white text-black p-2 rounded hover:text-black mx-2 cursor-pointer`: `p-2 rounded hover:bg-white hover:text-black mx-2 cursor-pointer`}>How to </li></Link>
-			<Link href={'/usefull-websites/'}><li className={router.pathname == '/usefull-websites'?`bg-white text-black p-2 rounded hover:text-black mx-2 cursor-pointer`: `p-2 rounded hover:bg-white hover:text-black mx-2 cursor-pointer`}>Usefull Websites</li></Link>
-			<Link href={'/usefull-apps/'}><li className={router.pathname == '/usefull-apps'?`bg-white text-black p-2 rounded hover:text-black mx-2 cursor-pointer`: `p-2 rounded hover:bg-white hover:text-black mx-2 cursor-pointer`}>Usefull Apps</li></Link>
+			<Link href={'/tech-news/'}><li onClick={(e)=>{setHamburger(false)}} className={router.pathname == '/tech-news'?`bg-white text-black p-2 rounded hover:text-black mx-2 cursor-pointer`: `p-2 rounded hover:bg-white hover:text-black mx-2 cursor-pointer`}>Tech-News</li></Link>
+			<Link href={'/how-to/'}><li onClick={(e)=>{setHamburger(false)}} className={router.pathname == '/how-to'?`bg-white text-black p-2 rounded hover:text-black mx-2 cursor-pointer`: `p-2 rounded hover:bg-white hover:text-black mx-2 cursor-pointer`}>How-to </li></Link>
+			<Link href={'/usefull-websites/'}><li onClick={(e)=>{setHamburger(false)}} className={router.pathname == '/usefull-websites'?`bg-white text-black p-2 rounded hover:text-black mx-2 cursor-pointer`: `p-2 rounded hover:bg-white hover:text-black mx-2 cursor-pointer`}>Usefull-Websites</li></Link>
+			<Link href={'/usefull-apps/'}><li onClick={(e)=>{setHamburger(false)}} className={router.pathname == '/usefull-apps'?`bg-white text-black p-2 rounded hover:text-black mx-2 cursor-pointer`: `p-2 rounded hover:bg-white hover:text-black mx-2 cursor-pointer`}>Usefull-Apps</li></Link>
 		</ul>
 	</>
 }
