707fe555b78aad982b09aedf398b93e5740d5890
codersharon <124261315+codersharon@users.noreply.github.com>
2023-05-29 13:07:16 +0000

    insta-id-updated

--- pages/comps/footer.tsx
+++ pages/comps/footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
       <Link href={'/about'}>About me</Link>
     </p>
     <span className="sm:mx-2 inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-      <a href="https://www.instagram.com/_sh.xron._/" target="_blank" rel="noreferrer" className="ml-3 text-gray-400">
+      <a href="https://www.instagram.com/sh.xr.on/" target="_blank" rel="noreferrer" className="ml-3 text-gray-400">
         <svg fill="none" stroke="currentColor" strokelincap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
           <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
           <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
