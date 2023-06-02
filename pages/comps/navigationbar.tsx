5aff3bfd30bb3b892c8cdf4085a0dc075444f2cc
codersharon <124261315+codersharon@users.noreply.github.com>
2023-06-02 14:34:23 +0000

    Done

--- pages/comps/navigationbar.tsx
+++ pages/comps/navigationbar.tsx
@@ -5,14 +5,12 @@ const NavigationBar = () => {
 	const router = useRouter();
 	
 	return <>
-		<nav className='lg:hidden p-2 bg-white sticky bottom-0 flex items-center justify-center z-10'>
-			<ul className='flex items-center justify-center'>
-				<Link href={'/'} ><img src='/home.svg' alt='home' className={router.pathname == '/' ? `mx-2 w-10 border-4 border-black rounded-lg` : 'mx-2 w-10'} /></Link>
-				<Link href={'/tech-news'} ><img src='/feed.svg' alt='feed' className={router.pathname == '/tech-news' ? `mx-2 w-10 border-4 border-black rounded-lg` : 'mx-2 w-10'} /></Link>
-				<Link href={'/usefull-websites'} ><img src='/websites.svg' alt='website' className={router.pathname == '/usefull-websites' ? `mx-2 w-10 border-4 border-black rounded-lg` : 'mx-2 w-10'} /></Link>
-				<Link href={'/usefull-apps'} ><img src='/apps.svg' alt='apps' className={router.pathname == '/usefull-apps' ? `mx-2 w-10 border-4 border-black rounded-lg` : 'mx-2 w-10'} /></Link>
-				<Link href={'/about'} ><img src='/about.svg' alt='about' className={router.pathname == '/about' ? `mx-2 w-10 border-4 border-black rounded-lg` : 'mx-2 w-10'} /></Link>
-			 ?</ul>
+		<nav className='w-full mx-1 lg:hidden p-2 bg-white sticky bottom-0 flex items-center justify-center z-10'>
+			<Link href={'/'} ><img src='/home.svg' alt='home' className={router.pathname == '/' ? `mx-5 w-8 border-4 border-black rounded-lg` : 'mx-5 w-8'} /></Link>
+			<Link href={'/tech-news'} ><img src='/feed.svg' alt='feed' className={router.pathname == '/tech-news' ? `mx-5 w-8 border-4 border-black rounded-lg` : 'mx-5 w-8'} /></Link>
+			<Link href={'/usefull-websites'} ><img src='/websites.svg' alt='website' className={router.pathname == '/usefull-websites' ? `mx-5 w-8 border-4 border-black rounded-lg` : 'mx-5 w-8'} /></Link>
+			<Link href={'/usefull-apps'} ><img src='/apps.svg' alt='apps' className={router.pathname == '/usefull-apps' ? `mx-5 w-8 border-4 border-black rounded-lg` : 'mx-5 w-8'} /></Link>
+			<Link href={'/about'} ><img src='/about.svg' alt='about' className={router.pathname == '/about' ? `mx-5 w-8 border-4 border-black rounded-lg` : 'mx-5 w-8'} /></Link>
 		</nav>
 	</>
 }
