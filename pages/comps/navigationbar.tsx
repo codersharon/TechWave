1e1e918a2619b7d31bfefed8a8980e0f9d187bf2
codersharon <124261315+codersharon@users.noreply.github.com>
2023-06-02 13:54:13 +0000

    ts5

--- pages/comps/navigationbar.tsx
+++ pages/comps/navigationbar.tsx
@@ -8,7 +8,6 @@ const NavigationBar = () => {
 		<nav className='lg:hidden p-2 bg-white sticky bottom-0 flex items-center justify-center z-10'>
 			<ul className='flex items-center justify-center'>
 				<Link href={'/'} ><img src='/home.svg' alt='home' className={router.pathname == '/' ? `mx-2 w-10 border-4 border-black rounded-lg` : 'mx-2 w-10'} /></Link>
-				<Link href={'/search'} ><img src='/search.svg' alt='search' className={router.pathname == '/search' ? `mx-2 w-10 border-4 border-black rounded-lg` : 'mx-2 w-10'} /></Link>
 				<Link href={'/tech-news'} ><img src='/feed.svg' alt='feed' className={router.pathname == '/tech-news' ? `mx-2 w-10 border-4 border-black rounded-lg` : 'mx-2 w-10'} /></Link>
 				<Link href={'/usefull-websites'} ><img src='/websites.svg' alt='website' className={router.pathname == '/usefull-websites' ? `mx-2 w-10 border-4 border-black rounded-lg` : 'mx-2 w-10'} /></Link>
 				<Link href={'/usefull-apps'} ><img src='/apps.svg' alt='apps' className={router.pathname == '/usefull-apps' ? `mx-2 w-10 border-4 border-black rounded-lg` : 'mx-2 w-10'} /></Link>
